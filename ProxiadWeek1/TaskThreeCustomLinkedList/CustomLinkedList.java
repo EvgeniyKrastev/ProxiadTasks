846924a6a524a2028eb21229cba08fe251726633
Evgeniy <mostwanted82896@gmail.com>
2025-01-06 16:20:17 +0200

    with deleted comment

--- ProxiadWeek1/TaskThreeCustomLinkedList/CustomLinkedList.java
+++ ProxiadWeek1/TaskThreeCustomLinkedList/CustomLinkedList.java
@@ -1,4 +1,24 @@
 package ProxiadWeek1.TaskThreeCustomLinkedList;
 
-public class CustomLinkedList {
+//Задача 3 - CustomLinkedList
+//
+//Да се имплементира свързан списък, подобен на LinkedList в Java. Трябва да съдържа следните публични методи:
+//
+//  + count() – int;
+//	+ addFirst(E element) – void;
+//	+ removeFirst() – E;
+
+class CustomLinkedList {
+
+    public CustomLinkedList() {
+    }
+
+    public int count(){
+
+    }
+}
+
+class Node<T> {
+    public T data;
+    public Node<T> next;
 }
